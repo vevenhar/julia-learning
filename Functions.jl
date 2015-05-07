0283af604838b4cbe0adb0a49aa1196c28bc4518
Vlad Evenhar <vevenhar@gmail.com>
2015-05-07 14:53:50 -0500

    more functions

--- Functions.jl
+++ Functions.jl
@@ -8,3 +8,39 @@ end
 
 # factorial
 fact(n) = n > 0 ? n * fact(n - 1) : n == 0 ? 1 : error("n cannot be negative")
+
+# factorial without recursion
+function fact2(n)
+  x = 1; 
+  for i in 1:n
+    x *= i; 
+    println(x)
+  end
+end
+
+f(x) = x^3 + 2x + 1
+
+# calculate powers of ten
+for n in 0:10
+  println(10^n)
+end
+
+# calculate powers of two
+for n in 0:10
+  println(2^n)
+end
+
+# Fibonacci numbers, x is how many numbers to generate and print
+function fib(x)
+  prev = 1;
+  next = 1;
+  println(prev)
+  println(next)
+  for n in 1:x-2
+    tmp = next
+    next = prev + next
+    prev = tmp
+    println(next)
+  end
+end
+
